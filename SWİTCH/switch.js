d98dcfeb31fdb1d451698918fde40d93f309e0fe
ummet <ummetaydin080@gmail.com>
2023-01-19 01:16:48 +0300

    switch done

---
+++ SWİTCH/switch.js
@@ -0,0 +1,24 @@
+//switch case
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+// let category = "beyaz eşya";
+
+// switch (category) {
+//     case "telefon":
+//         console.log("telefon kategorisi");
+//         break;
+
+//     case "bilgisayar":
+//         console.log("bilgisayar kategorisi");
+//         break;
+//     default:
+//         console.log('yanlış kategori');
+
+// }
