b437eb93aec5423d79c96cf4818f7f4045b9c9df
ummet <ummetaydin080@gmail.com>
2023-01-19 01:01:48 +0300

    if else

---
+++ ARRAY/İF ELSE/ifelse.js
@@ -0,0 +1,119 @@
+// const firstName='Sena';
+// const age=19;
+ // const isStudent= true;
+// const school= 'university';
+
+// if(firstName=='Sena'){
+//     console.log('Merhaba Sena');// Merhaba Sena
+// }
+
+
+// if(age==19){
+//     console.log('yaşınız 19 olsun'); //yaşınız 19 olsun
+// }
+
+// if(isStudent){
+// console.log('Öğrenci misin?');// Öğrenci misin?
+// }
+
+
+// if(isStudent==true){
+//     console.log('doğru cevap');// doğru cevap
+// }
+    
+// const isStudent= false;
+
+// if (isStudent){
+//     console.log('Merhaba ögrenci');
+// }else{
+//     console.log('ögrenci degilim'); //ögrenci degilim
+// } 
+
+// if(age>=18) {
+//     if(school=='university'){
+//         console.log('ehliyet alabilirsiniz');
+
+//     }else{
+//         console.log('egitim durumunuz yetersiz');
+//    }
+
+
+
+// }else{
+//     console.log('ehliyet alamazsınız');
+// }
+
+
+//  ehliyet alabilirsiniz              ifelse.js:34
+
+
+// const firstName='Sena';
+// const age=19;
+// const isStudent= true;
+// const school= 'hight scholl';
+
+
+// if(age>=18) {
+//     if(school=='university'){
+//         console.log('ehliyet alabilirsiniz');
+
+//     }else{
+//         console.log('egitim durumunuz yetersiz'); //egitim durumunuz yetersiz
+//    }
+
+
+
+// }else{
+//     console.log('ehliyet alamazsınız');
+// }
+
+// const firstName='Sena';
+// const age=19;
+// const isStudent= true;
+// const school= 'hight scholl';
+
+
+// if(age>=18) {
+//     if((school=='university') || (school =='hight scholl')){
+//         console.log('ehliyet alabilirsiniz'); 
+
+//     }else{
+//         console.log('egitim durumunuz yetersiz');
+//    }
+
+
+// }else{
+//     console.log('ehliyet alamazsınız');
+// }
+
+//ehliyet alabilirsiniz
+
+// const firstName='Sena';
+// const age=19;
+// const isStudent= true;
+// const school= 'hight scholl';
+
+// if(age>0 && age<12){
+//     console.log(`${firstname} is a child`);
+    
+// }else if(age>=13 && age<=19){
+//     console.log(`${firstName} is a teenager`);
+
+// }else{
+//     console.log(`${firstName} is a adult`);
+// }
+
+//Sena is a teenager
+
+
+// if(typeof id!=='undefined'){
+
+//     console.log('id:' +id);
+
+// }else{
+
+//     console.log('no id');
+
+// }
+
+//no id
