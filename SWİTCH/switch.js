62bbc83230923b9fea67c3574149745f7af27bfb
ummet <ummetaydin080@gmail.com>
2023-01-20 10:08:40 +0300

    object

--- SWİTCH/switch.js
+++ SWİTCH/switch.js
@@ -22,3 +22,42 @@
 //         console.log('yanlış kategori');
 
 // }
+
+// let val;
+
+// let person={
+//     firstName:'Fatih',
+//     lastName:'Aydin',
+// };
+// console.log(person);// {firstName: 'Fatih', lastName: 'Aydin'}
+
+// console.log(typeof person); //object
+
+
+
+let val;
+
+let person={
+    firstName:'Fatih',
+    lastName:'Aydin',
+    age:7,
+    hobbies:['music', 'game'],
+    adress:{
+        city:'Osmaniye',
+        country:'Türkiye',
+    }
+    
+};
+
+// console.log(typeof person); //object
+
+val=person;
+val=person.firstName;//Aydın
+val=person.lastName;//Aydın
+val=person['firstName'];// Fatih
+val=person.age; //7
+val=person.hobbies; //['music', 'game']
+val=person.adress; //{city: 'Osmaniye', country: 'Türkiye'}
+val=person.adress[0];
+
+console.log(val);// 
